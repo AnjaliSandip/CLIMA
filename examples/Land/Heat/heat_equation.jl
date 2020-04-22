a76a16733d6cedd76a08d535c21f29a5f0919d02
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-04-22 16:31:44 -0700

    Add heat equation example

---
+++ examples/Land/Heat/heat_equation.jl
@@ -0,0 +1,173 @@
+# --------------------------------- run CLIMA SOIL MODEL -----------------------
+# CLIMA_run_SoilHeat.jl: This model simulates soil heat dynamics for the CliMA model
+
+######
+###### 1) Import/Export Needed Functions
+######
+println("1) Import/Export Needed Functions")
+
+# Load necessary CliMA subroutines
+using MPI
+using Test
+using CLIMA
+using Logging
+using Printf
+using NCDatasets
+using LinearAlgebra
+using OrderedCollections
+using CLIMA.Mesh.Topologies
+using CLIMA.Mesh.Grids
+using CLIMA.Writers
+using CLIMA.VTK
+using CLIMA.Mesh.Elements: interpolationmatrix
+using CLIMA.DGmethods
+using CLIMA.DGmethods.NumericalFluxes
+using CLIMA.MPIStateArrays
+using CLIMA.GenericCallbacks: EveryXWallTimeSeconds, EveryXSimulationSteps
+using CLIMA.GenericCallbacks
+using CLIMA.ODESolvers
+
+using Interpolations
+using DelimitedFiles
+
+# ENV["GKS_ENCODING"] = "utf-8"
+
+ENV["CLIMA_GPU"] = "false"
+
+# Initialize CliMA
+CLIMA.init()
+
+FT = Float64
+
+# Change output directory and save plots there
+output_dir = joinpath(dirname(dirname(pathof(CLIMA))), "output", "land")
+mkpath(output_dir)
+
+# Add soil model
+include("heat_equation_model.jl")
+
+######
+###### Include helper and plotting functions (to be refactored/moved into CLIMA src)
+######
+
+function get_z(grid)
+    # TODO: this currently uses some internals: provide a better way to do this
+    return reshape(grid.vgeo[(1:(N+1)^2:(N+1)^3),CLIMA.Mesh.Grids.vgeoid.x3id,:],:)*100
+end
+include(joinpath("..","helper_funcs.jl"))
+include(joinpath("..","plotting_funcs.jl"))
+
+######
+###### 2) Set up domain
+######
+println("2) Set up domain...")
+
+# NOTE: this is using 5 vertical elements, each with a 5th degree polynomial,
+# giving an approximate resolution of 5cm
+const velems = collect(1:10) # Elements at: [0.0 -0.2 -0.4 -0.6 -0.8 -1.0] (m)
+const N = 5 # Order of polynomial function between each element
+
+# Set domain using Stached Brick Topology
+topl = StackedBrickTopology(MPI.COMM_WORLD, (0.0:1,0.0:1,velems);
+    periodicity = (true,true,false),
+    boundary=((0,0),(0,0),(1,2)))
+grid = DiscontinuousSpectralElementGrid(topl, FloatType = Float64, DeviceArray = Array, polynomialorder = N)
+
+# Create Model struct
+m = HeatModel(
+    # Define heat capacity of soil
+    ρc = (state, aux, t) ->  1,
+
+    # Define thermal conductivity of soil
+    α  = (state, aux, t) ->  10,
+
+    # Define initial temperature of soil
+    initialT = (aux, t) -> (273.15 + 22.0),
+
+    # Define surface boundary condition
+    surfaceT = (state, aux, t) -> 300.0 # replace with T_data
+)
+
+# Set up DG scheme
+dg = DGModel( #
+  m, # "PDE part"
+  grid,
+  CentralNumericalFluxNonDiffusive(), # penalty terms for discretizations
+  CentralNumericalFluxDiffusive(),
+  CentralNumericalFluxGradient())
+
+# Minimum spatial and temporal steps
+Δ = min_node_distance(grid)
+CFL_bound = (Δ^2 / (2 * 2.42/2.49e6))
+dt = CFL_bound*0.5 # TODO: provide a "default" timestep based on  Δx,Δy,Δz
+
+
+######
+###### 3) Define variables for simulation
+######
+println("3) Define variables for simulation...")
+
+# Define time variables
+const minute = 60
+const hour = 60*minute
+const day = 24*hour
+# const timeend = 1*minute
+# const n_outputs = 25
+const timeend = 5*day
+
+# Output frequency:
+# const every_x_simulation_time = ceil(Int, timeend/n_outputs)
+const every_x_simulation_time = 1*day
+
+
+######
+###### 4) Prep ICs, and time-stepper and output configurations
+######
+println("4) Prep ICs, and time-stepper and output configurations...")
+
+# state variable
+Q = init_ode_state(dg, Float64(0))
+
+# initialize ODE solver
+lsrk = LSRK54CarpenterKennedy(dg, Q; dt = dt, t0 = 0)
+
+# Plot initial state
+p = get_plot(grid, Q, dg.auxstate, 0)
+export_plots(p, joinpath(output_dir, "initial_state.png"))
+
+mkpath(output_dir)
+
+plots = []
+dims = OrderedDict("z" => collect(get_z(grid)))
+# run for 8 days (hours?) to get to steady state
+
+output_data = DataFile(joinpath(output_dir, "output_data"))
+
+step = [0]
+stcb = GenericCallbacks.EveryXSimulationTime(every_x_simulation_time, lsrk) do (init = false)
+  state_vars = get_vars_from_stack(grid, Q, m, vars_state; exclude=["θi"])
+  aux_vars = get_vars_from_stack(grid, dg.auxstate, m, vars_aux; exclude=["z","ψ"])
+  all_vars = OrderedDict(state_vars..., aux_vars...)
+  write_data(NetCDFWriter(), output_data(step[1]), dims, all_vars, gettime(lsrk))
+  step[1]+=1
+  nothing
+end
+
+
+######
+###### 5) Solve the equations
+######
+println("5) Solve the equations...")
+
+solve!(Q, lsrk; timeend=timeend, callbacks=(stcb,))
+
+#####
+##### 6) Post-processing
+#####
+println("6) Post-processing...")
+
+all_data = collect_data(output_data, step[1])
+
+# To get "T" at timestep 0:
+
+
---
+++ examples/Land/Heat/heat_equation_model.jl
@@ -0,0 +1,179 @@
+# --------------------------------- CLIMA SOIL MODEL -----------------------
+# CLIMA_SoilHeat.jl: This model simulates soil heat dynamics for the CliMA model
+
+"""
+Soil Heat Model
+Computes diffusive flux `F` in:
+∂y / ∂t = ∇ ⋅ Flux + Source
+```
+  ∂T     ∂      ∂T
+------ = --(α * --)
+  ∂t     ∂z     ∂z
+```
+where
+ - `α` is the thermal conductivity (W/(m K))
+To write this in the form
+```
+∂Y
+-- + ∇⋅F(Y,t) = 0
+∂t
+```
+we write `Y = T` and `F(Y, t) = -α ∇T`.
+"""
+
+# --------------------------------- 1) Import/Export Needed Functions -----------------------
+
+# Add necessary CliMA functions and sub-routines
+using StaticArrays
+using CLIMA.VariableTemplates
+import CLIMA.DGmethods: BalanceLaw,
+                        vars_aux, vars_state, vars_gradient, vars_diffusive,
+                        flux_nondiffusive!, flux_diffusive!, source!,
+                        gradvariables!, diffusive!, update_aux!, nodal_update_aux!,
+                        init_aux!, init_state!,
+                        boundary_state!, wavespeed, LocalGeometry
+
+
+# --------------------------------- 2) Define Structs ---------------------------------------
+
+# Introduce needed variables into HeatModel struct
+Base.@kwdef struct HeatModel{Fρc, Fα, FiT, Fst} <: BalanceLaw
+
+  # Define heat capacity. This is an input to the model now.
+  ρc::Fρc       = (state, aux, t) -> 1   # [ Sand: ρc = 2.49e6 J m-3 K-1 ; Clay: ρc = 2.61e6 J m-3 K-1 ]
+  # Replace this with a function that calculates heat capacity (based on liquid+ice)
+  # OR Replace this with tabulated values of heat capacity (based on liquid+ice)
+
+  # Define kappa (thermal conductivity). This is an input to the model now.
+  α::Fα         = (state, aux, t) -> 10     # [ Sand: λ = 2.42 W m-1 K-1 ; Clay: λ = 1.17 W m-1 K-1 ]
+
+  # Define initial and boundary condition parameters
+  initialT::FiT = (aux, t) -> 273.15 + 2.0 # Initial Temperature. This is an input to the model now.
+  surfaceT::Fst = (state, aux, t) -> (273.15 + 2.0) # Surface boundary condition. This is an input to the model now.
+end
+
+
+# --------------------------------- 3) Define CliMA vars ---------------------------------------
+
+# Stored in the aux state are:
+#   `coord` coordinate points (needed for BCs)
+#   `u` advection velocity
+#   `D` Diffusion tensor
+vars_aux(::HeatModel, FT) = @vars(z::FT, T::FT) # stored dg.auxstate
+vars_state(::HeatModel, FT) = @vars(ρcT::FT, q_tot::FT) # stored in Q
+vars_gradient(::HeatModel, FT) = @vars(T::FT) # not stored
+vars_diffusive(::HeatModel, FT) = @vars(∇T::SVector{3,FT}) # stored in dg.diffstate
+
+# --------------------------------- 4) CliMA functions needed for simulation -------------------
+
+# ---------------- 4a) Update states
+
+# Update all auxiliary variables
+function update_aux!(
+    dg::DGModel,
+    m::HeatModel,
+    Q::MPIStateArray,
+    t::Real,
+    elems::UnitRange,
+)
+  nodal_update_aux!(soil_nodal_update_aux!, dg, m, Q, t, elems)
+  return true
+end
+# Update all auxiliary nodes
+function soil_nodal_update_aux!(
+  m::HeatModel,
+  state::Vars,
+  aux::Vars,
+  t::Real)
+  aux.T = state.ρcT / m.ρc(state, aux, t)
+end
+
+# ---------------- 4b) Calculate state and derivative of T
+
+# Calculate T based on internal energy state variable
+function gradvariables!(
+    m::HeatModel,
+    transform::Vars,
+    state::Vars,
+    aux::Vars,
+    t::Real,
+)
+  transform.T = state.ρcT / m.ρc(state, aux, t)
+end
+# Gradient of T calculation
+function diffusive!(
+    m::HeatModel,
+    diffusive::Vars,
+    ∇transform::Grad,
+    state::Vars,
+    aux::Vars,
+    t::Real,
+  )
+  diffusive.∇T = ∇transform.T
+end
+# Calculate thermal flux (non-diffusive (?))
+function flux_nondiffusive!(
+    m::HeatModel,
+    flux::Grad,
+    state::Vars,
+    aux::Vars,
+    t::Real,
+  )
+end
+# Calculate thermal flux (diffusive (?))
+function flux_diffusive!(
+    m::HeatModel,
+    flux::Grad,
+    state::Vars,
+    diffusive::Vars,
+    hyperdiffusive::Vars,
+    aux::Vars,
+    t::Real,
+  )
+   flux.ρcT -= m.α(state, aux, t) * diffusive.∇T
+end
+
+# ---------------- 4c) Extra Sources
+
+# Introduce sources of energy (e.g. Metabolic heat from microbes)
+function source!(m::HeatModel, state::Vars, _...)
+end
+
+# ---------------- 4d) Initialization
+
+# Initialize z-Profile
+function init_aux!(m::HeatModel, aux::Vars, geom::LocalGeometry)
+  aux.z = geom.coord[3]
+  aux.T = m.initialT(aux, 0)
+end
+
+# Initialize State variables from T to internal energy
+function init_state!(m::HeatModel, state::Vars, aux::Vars, coords, t::Real)
+  state.ρcT = m.ρc(state, aux, t) * aux.T
+end
+
+# ---------------- 4e) Boundary Conditions
+
+# Boundary condition function
+function boundary_state!(nf, m::HeatModel, state⁺::Vars, aux⁺::Vars,
+                         nM, state⁻::Vars, aux⁻::Vars, bctype, t, _...)
+  if bctype == 1
+    # surface
+    state⁺.ρcT = m.ρc(state⁻, aux⁻, t) * m.surfaceT(state⁻, aux⁻, t)
+  elseif bctype == 2
+    # bottom
+    nothing
+  end
+end
+# Boundary condition function - repeated?
+function boundary_state!(nf, m::HeatModel, state⁺::Vars, diff⁺::Vars,
+                         aux⁺::Vars, nM, state⁻::Vars, diff⁻::Vars, aux⁻::Vars,
+                         bctype, t, _...)
+  if bctype == 1
+    # surface
+    state⁺.ρcT = m.ρc(state⁻, aux⁻, t) * m.surfaceT(state⁻, aux⁻, t)
+  elseif bctype == 2
+    # bottom
+    diff⁺.∇T = -diff⁻.∇T
+  end
+end
